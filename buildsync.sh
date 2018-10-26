e2e252b46010d1befc7c9778b5dc8dea9e823544
serves <serves@serves.home>
2018-10-26 23:51:41 +0000

    update sync build script

--- buildsync.sh
+++ buildsync.sh
@@ -5,7 +5,7 @@ cd ~/nexplorer/
 git pull
 cd Nexplorer.Sync
 sudo dotnet publish -c Release
-sudo cp ~/.nexplorer/connectionStrings.json ~/nexplorer/Nexplorer.Sync/bin/Debug/netcoreapp2.1
-sudo cp ~/.nexplorer/emailConfig.json ~/nexplorer/Nexplorer.Sync/bin/Debug/netcoreapp2.1
+sudo cp ~/.nexplorer/connectionStrings.json ~/nexplorer/Nexplorer.Sync/bin/Release/netcoreapp2.1
+sudo cp ~/.nexplorer/emailConfig.json ~/nexplorer/Nexplorer.Sync/bin/Release/netcoreapp2.1
 sudo supervisorctl start nexplorerSync
 echo "Nexplorer sync is running!"
