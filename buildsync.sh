fe5b40f23f9e3e300c3e9ac353d7a826a8f76330
boxsie <dan@boxsie.co.uk>
2018-11-01 18:20:30 +0000

    update build scripts

--- buildsync.sh
+++ buildsync.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-sudo supervisorctl stop nexplorerSync
 cd ~/nexplorer/
 git pull
 cd Nexplorer.Sync
 sudo dotnet publish -c Release
 sudo cp ~/.nexplorer/connectionStrings.json ~/nexplorer/Nexplorer.Sync/bin/Release/netcoreapp2.1/publish/connectionStrings.json
 sudo cp ~/.nexplorer/emailConfig.json ~/nexplorer/Nexplorer.Sync/bin/Release/netcoreapp2.1/publish/emailConfig.json
-sudo supervisorctl start nexplorerSync
+sudo cp ~/.nexplorer/userConfig.json ~/nexplorer/Nexplorer.Sync/bin/Release/netcoreapp2.1/publish/userConfig.json
+sudo rm -rf /var/nexplorerSync
+sudo cp -r ~/nexplorer/Nexplorer.Sync/bin/Release/netcoreapp2.1/publish/ /var/nexplorerSync/
+sudo supervisorctl restart nexplorerSync
 echo "Nexplorer sync is running!"
