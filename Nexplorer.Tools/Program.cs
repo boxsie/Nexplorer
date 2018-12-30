d4c6a291f9894db67d9f31a09362b517c0c31d8b
danielsan <dan@boxsie.co.uk>
2018-12-30 00:19:51 +0000

    updated nLog

--- Nexplorer.Tools/Program.cs
+++ Nexplorer.Tools/Program.cs
@@ -15,14 +15,36 @@ namespace Nexplorer.Tools
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Run();
+            // NLog: setup the logger first to catch all errors
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("init main");
+                BuildWebHost(args).Run();
+            }
+            catch (Exception ex)
+            {
+                //NLog: catch setup errors
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
         }
 
-        private static IWebHost CreateWebHostBuilder(string[] args) =>
+        private static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseKestrel()
                 .UseUrls("http://localhost:5005")
                 .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Information);
+                })
                 .UseNLog()
                 .Build();
     }
