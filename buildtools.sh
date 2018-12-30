dd7f088771f29482fbdfce025d493d5a566078b0
danielsan <dan@boxsie.co.uk>
2018-12-30 00:30:25 +0000

    update build scripts

--- buildtools.sh
+++ buildtools.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
 cd ~/Nexplorer/
-#git pull
-#cd Nexplorer.Web/wwwroot
-#sudo rm -rf css
-#sudo rm -rf js
-#sudo rm -rf font
-#cd ..
-#sudo npm install
-#sudo npm run build
+git pull
 sudo dotnet publish -c Release
 sudo rm -rf /var/www/nexplorerTools/
 sudo cp -r ~/Nexplorer/Nexplorer.Tools/bin/Release/netcoreapp2.2/publish/ /var/www/nexplorerTools
 sudo cp ~/.Nexplorer/connectionStrings.json /var/www/nexplorerTools
 sudo cp ~/.Nexplorer/emailConfig.json /var/www/nexplorerTools
+sudo chgrp -R www-data /var/www/nexplorerTools/
+sudo chmod -R g+w /var/www/nexplorerTools/
 sudo supervisorctl restart nexplorerTools
 echo "Nexplorer web is running!"
