5e61ef8d84713b1ee7058ef1533de959e009555d
danielsan <dan@boxsie.co.uk>
2019-02-03 10:03:42 +0000

    added a wait for the node

--- Nexplorer.Sync/App.cs
+++ Nexplorer.Sync/App.cs
@@ -14,13 +14,15 @@ namespace Nexplorer.Sync
 {
     public class App
     {
+        private readonly ILogger<App> _logger;
         private readonly NexusQuery _nexusQuery;
         private readonly BlockSyncCatchup _blockCatchup;
         private readonly AddressAggregateCatchup _addressCatchup;
         private readonly RedisCommand _redisCommand;
 
-        public App(NexusQuery nexusQuery, BlockSyncCatchup blockCatchup, AddressAggregateCatchup addressCatchup, RedisCommand redisCommand)
+        public App(ILogger<App> logger, NexusQuery nexusQuery, BlockSyncCatchup blockCatchup, AddressAggregateCatchup addressCatchup, RedisCommand redisCommand)
         {
+            _logger = logger;
             _nexusQuery = nexusQuery;
             _blockCatchup = blockCatchup;
             _addressCatchup = addressCatchup;
@@ -29,21 +31,42 @@ namespace Nexplorer.Sync
 
         public async Task StartAsync()
         {
-            await _blockCatchup.CatchupAsync();
-            await _addressCatchup.CatchupAsync();
+            if (await EnsureNodeIsReady())
+            {
+                await _blockCatchup.CatchupAsync();
+                await _addressCatchup.CatchupAsync();
 
-            await _redisCommand.SetAsync(Settings.Redis.NodeVersion, (await _nexusQuery.GetInfoAsync()).Version);
-            
-            await Task.WhenAll(
-                JobService.StartJob(typeof(BlockScanJob)),
-                JobService.StartJob(typeof(TimestampSyncJob)),
-                JobService.StartJob(typeof(ExchangeSyncJob)),
-                JobService.StartJob(typeof(MiningInfoSyncJob)),
-                JobService.StartJob(typeof(BlockSyncJob), 20),
-                JobService.StartJob(typeof(NexusAddressCacheJob), 20),
-                JobService.StartJob(typeof(TrustAddressCacheJob), 40),
-                JobService.StartJob(typeof(MiningStatsJob), 40),
-                JobService.StartJob(typeof(AddressStatsJob), 60));
+                await Task.WhenAll(
+                    JobService.StartJob(typeof(BlockScanJob)),
+                    JobService.StartJob(typeof(TimestampSyncJob)),
+                    JobService.StartJob(typeof(ExchangeSyncJob)),
+                    JobService.StartJob(typeof(MiningInfoSyncJob)),
+                    JobService.StartJob(typeof(BlockSyncJob), 20),
+                    JobService.StartJob(typeof(NexusAddressCacheJob), 20),
+                    JobService.StartJob(typeof(TrustAddressCacheJob), 40),
+                    JobService.StartJob(typeof(MiningStatsJob), 40),
+                    JobService.StartJob(typeof(AddressStatsJob), 60));
+            }
+        }
+
+        private async Task<bool> EnsureNodeIsReady()
+        {
+            var nxsVersion = await _nexusQuery.GetInfoAsync();
+
+            while (nxsVersion == null)
+            {
+                _logger.LogCritical("Nexus node is not available at this time.....");
+
+                nxsVersion = await _nexusQuery.GetInfoAsync();
+
+                await Task.Delay(TimeSpan.FromSeconds(10));
+            }
+
+            _logger.LogInformation($"Nexus node connected on version {nxsVersion.Version}");
+
+            await _redisCommand.SetAsync(Settings.Redis.NodeVersion, nxsVersion.Version);
+
+            return true;
         }
     }
 }
