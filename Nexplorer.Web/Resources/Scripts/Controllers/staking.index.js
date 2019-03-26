e544c96d47ba9a939085270110205b41f933d55e
danielsan <dan@boxsie.co.uk>
2019-03-26 13:55:38 +0000

    dpupdate

--- Nexplorer.Web/Resources/Scripts/Controllers/staking.index.js
+++ Nexplorer.Web/Resources/Scripts/Controllers/staking.index.js
@@ -1,6 +1,7 @@
 ﻿import Vue from 'vue';
 import { HubConnectionBuilder, LogLevel } from '@aspnet/signalr';
 
+import datePickerVue from '../Library/datePickerVue.js';
 import streamingLineChart from '../Library/streamingChart.js';
 import '../../Style/staking.index.scss';
 
@@ -9,10 +10,39 @@ export class StakingIndexViewModel {
         this.vm = new Vue({
             el: '#main',
             data: {
-                
+                ud: [
+                    {
+                        startDate: '27/03/2019',
+                        endDate: '30/03/2019'
+                    },
+                    {
+                        startDate: '02/04/2019',
+                        endDate: '03/04/2019'
+                    },
+                    {
+                        startDate: '03/04/2019',
+                        endDate: '04/04/2019'
+                    },
+                    {
+                        startDate: '04/04/2019',
+                        endDate: '07/04/2019'
+                    },
+                    {
+                        startDate: '09/04/2019',
+                        endDate: '16/04/2019'
+                    },
+                    {
+                        startDate: '19/04/2019',
+                        endDate: '22/04/2019'
+                    }
+                ],
+                dpOptions: {
+                    allowPast: false,
+                    maxDate: '11/08/2020'
+                }
             },
             components: {
-                
+                datePicker: datePickerVue
             },
             methods: {
                 
