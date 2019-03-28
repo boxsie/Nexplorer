610f2e29540a8b93aa3eff2b9bc8907ce519fdc0
danielsan <dan@boxsie.co.uk>
2019-03-28 20:28:15 +0000

    nav adjustments for mobile

--- Nexplorer.Web/Resources/Scripts/layout.js
+++ Nexplorer.Web/Resources/Scripts/layout.js
@@ -23,14 +23,9 @@ export class LayoutViewModel {
                 searchOpen: false,
                 userOpen: false,
                 searchFocusDelay: 400,
-                navTop: 0,
-                layoutTop: 0,
-                mainBodyTop: 0,
-                layoutHeight: 40,
-                lastScrollTop: null,
-                xsNav: false,
                 userSettings: options.userSettings,
-                lastPrice: 0
+                lastPrice: 0,
+                waitingForTimeout: false
             },
             computed: {
                 userMenuCss() {
@@ -38,56 +33,6 @@ export class LayoutViewModel {
                     const signInOut = this.isSignedIn ? 'signed-in' : '';
 
                     return openClose + ' ' + signInOut;
-                },
-                height() {
-                    this.pulseElement($(this.$refs.tickerHeight));
-
-                    if (!this.$layoutHub.latestBlock || !this.$layoutHub.latestBlock.height) {
-                        return ' - ';
-                    }
-
-                    return this.$layoutHub.latestBlock.height.toLocaleString();
-                },
-                heightUrl() {
-                    if (!this.$layoutHub.latestBlock || !this.$layoutHub.latestBlock.height) {
-                        return '';
-                    }
-
-                    return this.$layoutHub.latestBlock.height ? `/blocks/${this.$layoutHub.latestBlock.height}` : '';
-                },
-                price() {
-                    if (!this.$layoutHub.latestPrice || !this.$layoutHub.latestPrice.last || this.$layoutHub.latestPrice.last === this.lastPrice) {
-                        return this.lastPrice ? this.lastPrice : ' - ';
-                    }
-
-                    this.lastPrice = this.$layoutHub.latestPrice.last;
-                    this.pulseElement($(this.$refs.tickerPrice));
-
-                    return this.$layoutHub.latestPrice.last.toFixed(8);
-                },
-                diffPos() {
-                    if (!this.$layoutHub.latestDiffs || !this.$layoutHub.latestDiffs.pos) {
-                        return ' - ';
-                    }
-
-                    this.pulseElement($(this.$refs.tickerDiffPos));
-                    return this.parseDifficulty(this.$layoutHub.latestDiffs.pos);
-                },
-                diffHash() {
-                    if (!this.$layoutHub.latestDiffs || !this.$layoutHub.latestDiffs.hash) {
-                        return ' - ';
-                    }
-
-                    this.pulseElement($(this.$refs.tickerDiffHash));
-                    return this.parseDifficulty(this.$layoutHub.latestDiffs.hash);
-                },
-                diffPrime() {
-                    if (!this.$layoutHub.latestDiffs || !this.$layoutHub.latestDiffs.prime) {
-                        return ' - ';
-                    }
-
-                    this.pulseElement($(this.$refs.tickerDiffPrime));
-                    return this.parseDifficulty(this.$layoutHub.latestDiffs.prime);
                 }
             },
             methods: {
@@ -131,62 +76,6 @@ export class LayoutViewModel {
                         $(navLinksEl).collapse('hide');
                     }
                 },
-                onScroll(e) {
-                    if (!this.$refs.nav.clientHeight) {
-                        return;
-                    }
-
-                    const doc = document.documentElement;
-                    const left = (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0);
-                    const top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
-                    
-                    this.navTop += (this.lastScrollTop - top) * 0.25;
-
-                    if (top < this.$refs.nav.clientHeight && this.navTop > top) {
-                        this.navTop = -top;
-                    } else if (this.navTop > 0) {
-                        this.navTop = 0;
-                    } else if (this.navTop < -this.$refs.nav.clientHeight) {
-                        this.navTop = -this.$refs.nav.clientHeight;
-                    }
-
-                    if (top < this.mainBodyTop) {
-                        this.layoutTop = this.mainBodyTop - top;
-                    } else {
-                        this.layoutTop = -(this.navTop + this.$refs.nav.clientHeight);
-                    }
-
-                    this.lastScrollTop = top;
-                },
-                windowResize() {
-                    if (this.isBreakpoint('xs')) {
-                        this.xsNav = true;
-
-                        if (this.navExpanded) {
-                            this.navExpanded = false;
-                        }
-                    } else {
-                        this.xsNav = false;
-                    }
-
-                    const top = $('#mainBody').offset().top - this.layoutHeight;
-
-                    if (top !== this.mainBodyTop) {
-                        this.mainBodyTop = top;
-                        this.onScroll();
-                    }
-                },
-                parseDifficulty(diff) {
-                    return parseFloat(diff.toFixed(8)).toLocaleString(undefined, { 'minimumFractionDigits': 2, 'maximumFractionDigits': 6 });
-                },
-                pulseElement($elem) {
-                    $elem.addClass('pulse');
-
-                    setTimeout(() => {
-                            $elem.removeClass('pulse');
-                        },
-                        1000);
-                },
                 closeCookieWarning() {
                     $(this.$refs.cookieWarning).height(0);
 
@@ -197,36 +86,9 @@ export class LayoutViewModel {
                 }
             },
             created() {
-                window.addEventListener('resize', this.windowResize);
-                window.addEventListener('scroll', this.onScroll);
                 document.addEventListener('click', this.documentClick);
                 document.addEventListener('touchstart', this.documentClick);
-                
-                const self = this;
-
-                $(() => {
-                    var diffs = $(this.$refs.mobileDiffs).children();
-                    var i = 0;
-                    var currentDiff = diffs[i];
-
-                    setInterval(() => {
-                        $(currentDiff).fadeOut('fast', () => {
-                            i++;
-
-                            if (i >= diffs.length) {
-                                i = 0;
-                            }
-
-                            currentDiff = diffs[i];
-                            $(currentDiff).fadeIn('fast');
-                        });
-                    }, 5000);
-                });
             }
-        });
-
-        $(() => {
-            this.navVm.windowResize();
         });
     }
 }
--- Nexplorer.Web/Resources/Scripts/layoutHub.js
+++ Nexplorer.Web/Resources/Scripts/layoutHub.js
@@ -1,4 +1,5 @@
-﻿import Vue from 'vue';
+﻿import $ from 'jquery';
+import Vue from 'vue';
 import moment from 'moment';
 import { HubConnectionBuilder, LogLevel } from '@aspnet/signalr';
 
@@ -9,7 +10,65 @@ export default {
                 utcMoment: moment(),
                 latestBlock: {},
                 latestPrice: {},
-                latestDiffs: {}
+                latestDiffs: {},
+                navTop: 0,
+                layoutTop: null,
+                mainBodyTop: 0,
+                layoutHeight: 40,
+                layoutClass: 'position-absolute',
+                lastScrollTop: null
+            },
+            computed: {
+                height() {
+                    this.pulseElement($(this.$refs.tickerHeight));
+
+                    if (!this.$layoutHub.latestBlock || !this.$layoutHub.latestBlock.height) {
+                        return ' - ';
+                    }
+
+                    return this.$layoutHub.latestBlock.height.toLocaleString();
+                },
+                heightUrl() {
+                    if (!this.$layoutHub.latestBlock || !this.$layoutHub.latestBlock.height) {
+                        return '';
+                    }
+
+                    return this.$layoutHub.latestBlock.height ? `/blocks/${this.$layoutHub.latestBlock.height}` : '';
+                },
+                price() {
+                    if (!this.$layoutHub.latestPrice || !this.$layoutHub.latestPrice.last || this.$layoutHub.latestPrice.last === this.lastPrice) {
+                        return this.lastPrice ? this.lastPrice : ' - ';
+                    }
+
+                    this.lastPrice = this.$layoutHub.latestPrice.last;
+                    this.pulseElement($(this.$refs.tickerPrice));
+
+                    return this.$layoutHub.latestPrice.last.toFixed(8);
+                },
+                diffPos() {
+                    if (!this.$layoutHub.latestDiffs || !this.$layoutHub.latestDiffs.pos) {
+                        return ' - ';
+                    }
+
+                    this.pulseElement($(this.$refs.tickerDiffPos));
+                    return this.parseDifficulty(this.$layoutHub.latestDiffs.pos);
+                },
+                diffHash() {
+                    if (!this.$layoutHub.latestDiffs || !this.$layoutHub.latestDiffs.hash) {
+                        return ' - ';
+                    }
+
+                    this.pulseElement($(this.$refs.tickerDiffHash));
+                    return this.parseDifficulty(this.$layoutHub.latestDiffs.hash);
+                },
+                diffPrime() {
+                    if (!this.$layoutHub.latestDiffs || !this.$layoutHub.latestDiffs.prime) {
+                        return ' - ';
+                    }
+
+                    this.pulseElement($(this.$refs.tickerDiffPrime));
+                    return this.parseDifficulty(this.$layoutHub.latestDiffs.prime);
+                }
             },
             methods: {
                 parseBlockChannel(channel) {
@@ -23,9 +82,66 @@ export default {
                     if (bytes === 0) return '0 Byte';
                     const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
                     return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+                },
+                parseDifficulty(diff) {
+                    return parseFloat(diff.toFixed(8)).toLocaleString(undefined, { 'minimumFractionDigits': 2, 'maximumFractionDigits': 6 });
+                },
+                pulseElement($elem) {
+                    $elem.addClass('pulse');
+
+                    setTimeout(() => {
+                            $elem.removeClass('pulse');
+                        },
+                        1000);
+                },
+                onScroll(instant) {
+                    if (!this.waitingForTimeout && !instant) {
+                        this.waitingForTimeout = true;
+
+                        setTimeout(() => {
+                            this.waitingForTimeout = false;
+                        }, 50);
+
+                        return;
+                    }
+
+                    const doc = document.documentElement;
+                    const left = (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0);
+                    const top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+
+                    this.navTop += (this.lastScrollTop - top) * 0.25;
+
+                    if (top < options.navHeight && this.navTop > top) {
+                        this.navTop = -top;
+                    } else if (this.navTop > 0) {
+                        this.navTop = 0;
+                    } else if (this.navTop < -options.navHeight) {
+                        this.navTop = -options.navHeight;
+                    }
+
+                    if (top > this.mainBodyTop) {
+                        this.layoutTop = -(this.navTop + options.navHeight);
+                        this.layoutClass = 'position-fixed';
+                    } else {
+                        this.layoutTop = this.mainBodyTop;
+                        this.layoutClass = 'position-absolute';
+                    }
+
+                    this.lastScrollTop = top;
+                },
+                windowResize() {
+                    const top = $('#mainBody').offset().top - this.layoutHeight;
+
+                    if (top !== this.mainBodyTop) {
+                        this.mainBodyTop = top;
+                        this.onScroll(true);
+                    }
                 }
             },
             created() {
+                window.addEventListener('resize', this.windowResize);
+                window.addEventListener('scroll', this.onScroll);
+
                 this.connection = new HubConnectionBuilder()
                     .configureLogging(LogLevel.Information)
                     .withUrl('/layouthub').build();
@@ -72,6 +188,29 @@ export default {
                 setInterval(() => {
                     this.utcMoment = moment(this.utcMoment.add(1, 's'));
                 }, 1000);
+
+                const self = this;
+
+                $(() => {
+                    var diffs = $(this.$refs.mobileDiffs).children();
+                    var i = 0;
+                    var currentDiff = diffs[i];
+
+                    setInterval(() => {
+                        $(currentDiff).fadeOut('fast', () => {
+                            i++;
+
+                            if (i >= diffs.length) {
+                                i = 0;
+                            }
+
+                            currentDiff = diffs[i];
+                            $(currentDiff).fadeIn('fast');
+                        });
+                    }, 5000);
+
+                    self.windowResize();
+                });
             }
         });
     }
